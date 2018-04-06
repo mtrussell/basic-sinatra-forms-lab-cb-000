a19474c9c9e26f231e988d14975d3c8f5d21ea87
Matt Trussell <smtrussell@gmail.com>
2018-04-06 22:50:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
   post '/newteam' do
     @team_name = params['team_name']
     @coach = params['coach']
